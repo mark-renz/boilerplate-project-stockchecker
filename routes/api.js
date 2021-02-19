66c801bbb6be407da0356df280f20ec66ab957d0
mark-renz <markrenzmanalo@gmail.com>
2021-02-19 21:34:20 +0800

    added api call function

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,37 @@
 'use strict';
+const request = require('request');
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(function ({ query }, res){
+      const stocks = query.stock;
+      
+      console.log(stocks);
+      
+      stocks.forEach(symbol => {
+        makeApiCall(symbol)
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      })
+
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify({stockData:{}}));
     });
     
 };
+
+const makeApiCall = (symbol) => {
+  const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
+  
+  return new Promise ((resolve, reject) => {
+    request(url, { json: true }, 
+      (err, res, body) => {
+        err ? reject(err) : resolve(body.latestPrice);
+    });
+  });
+}
