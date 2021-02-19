df783422515606658264b91af07f2e8f679462ed
mark-renz <markrenzmanalo@gmail.com>
2021-02-19 19:28:05 +0800

    set env variables

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify({stockData:{}}));
     });
     
 };
