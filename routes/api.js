faa636cb25573d80f75d1b34e6784bff4847f5a6
mark-renz <markrenzmanalo@gmail.com>
2021-02-20 13:00:28 +0800

    fixed makeApiCall func

--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,35 @@
 'use strict';
-const request = require('request');
+const https = require('https');
+const axios = require('axios');
+
+async function makeApiCall(symbol) {
+  const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
+
+  const res = await axios.get(url);
+  const price = await res.data.latestPrice;
+  const data =  {stock:symbol, price:price, likes:1};
+  return data;
+}
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function ({ query }, res){
-      const stocks = query.stock;
-      
-      console.log(stocks);
-      
-      stocks.forEach(symbol => {
-        makeApiCall(symbol)
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        })
-      })
+      const stocks = Array.isArray(query.stock) ? query.stock : [query.stock];
+      const like = query.like;
+      let data = [];
 
-      res.setHeader('Content-Type', 'application/json');
-      res.send(JSON.stringify({stockData:{}}));
+      Promise.all(
+        stocks.map(
+          async symbol=>{
+            const price = await makeApiCall(symbol);
+            return price;
+      })
+      ).then(val=>{
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify({stockData:val}));
+      })
     });
     
 };
 
-const makeApiCall = (symbol) => {
-  const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
-  
-  return new Promise ((resolve, reject) => {
-    request(url, { json: true }, 
-      (err, res, body) => {
-        err ? reject(err) : resolve(body.latestPrice);
-    });
-  });
-}
