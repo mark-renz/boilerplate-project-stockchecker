f393296d6f44381f7b438037ea90c68f50d6bb1b
mark-renz <markrenzmanalo@gmail.com>
2021-02-20 13:21:51 +0800

    fixed stockdata return

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,25 @@
 'use strict';
-const https = require('https');
 const axios = require('axios');
 
-async function makeApiCall(symbol) {
+async function getPrice(symbol) {
   const url = `https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${symbol}/quote`;
 
   const res = await axios.get(url);
   const price = await res.data.latestPrice;
-  const data =  {stock:symbol, price:price, likes:1};
+  const data =  {stock:symbol, price:price};
   return data;
+}
+
+
+async function getLikes(symbol) {
+  return 1;
 }
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function ({ query }, res){
+      //if returns single item convert it to array state
       const stocks = Array.isArray(query.stock) ? query.stock : [query.stock];
       const like = query.like;
       let data = [];
@@ -22,10 +27,14 @@ module.exports = function (app) {
       Promise.all(
         stocks.map(
           async symbol=>{
-            const price = await makeApiCall(symbol);
+            const price = await getPrice(symbol);
+            const likes = await getLikes(symbol);
+            price.likes = likes;
             return price;
       })
       ).then(val=>{
+        //if single remove to array state
+        val.length > 1 ? val : val=val[0];
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify({stockData:val}));
       })
